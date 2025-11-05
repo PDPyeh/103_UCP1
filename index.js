03dba90e3d9afa716b7350974aff46fbae7ad6e9
Pradipa Y <pdpyyo@gmail.com>
2025-11-05 10:02:48 +0700

    menambahkan .env dan index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const db = require('./models');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
